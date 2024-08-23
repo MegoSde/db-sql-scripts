926383c4cc5202a75716f65add7ec90aa9945a95
Mads Ekman Gommesen <mego@sde.dk>
2024-08-23 12:43:38 +0200

    Add two POC sql files to prevent hacking

---
+++ MSSQL-bluehat-hacking/sql-injection.sql
@@ -0,0 +1,148 @@
+﻿--Login som dbo
+--Lav to tabeller med data som udgangspunkt
+CREATE table [users] (
+	[Name] varchar(100) PRIMARY KEY, 
+	[Password] varbinary(100) )
+INSERT INTO [users]([Name],[Password]) VALUES ('Mads', HASHBYTES('SHA2_512', 'password'));
+
+CREATE table [secrets] (
+	[Text] varchar(100))
+INSERT INTO [secrets]([Text]) VALUES ('Hemmelighed');
+--Create a test user
+
+CREATE USER [webtester] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+--Viser hvad der er lagt ind
+SELECT * FROM [users];
+SELECT * FROM [secrets];
+GO
+--Test 1.
+--Lav en SP der benytter parameter
+CREATE OR ALTER PROCEDURE ValidateLogin
+	@Name VARCHAR(100),
+	@Password VARCHAR(100)
+AS
+	SELECT count(*) FROM [users] WHERE [Name] = @Name AND [Password] = HASHBYTES('SHA2_512', @Password)
+GO
+
+--Giv adgang til SP
+GRANT EXECUTE ON dbo.ValidateLogin TO [webtester]
+--test adgang som webtester
+EXECUTE AS USER = 'webtester'
+
+EXEC dbo.ValidateLogin @Name = 'Mads', @Password = 'password' --Rigtig, return 1
+EXEC dbo.ValidateLogin @Name = 'Mads', @Password = 'hest' --Rigtig, return 0
+EXEC dbo.ValidateLogin @Name = 'Mads''--', @Password = 'hest' --Rigtig, return 0 SQL injection virker ikke
+
+REVERT
+GO
+
+--Test 2. Lav en usikker SP
+CREATE OR ALTER PROCEDURE ValidateLoginSQL
+	@Name VARCHAR(100),
+	@Password VARCHAR(100) 
+AS
+	DECLARE @SQL NVARCHAR(4000) = '';
+	SELECT @SQL = 'SELECT COUNT(*) FROM [users] WHERE [Name] = ''' + @Name + ''' AND [Password] = HASHBYTES(''SHA2_512'',''' + @Password + ''')';
+	PRINT @SQL
+    EXEC sp_executesql @SQL;
+GO
+--test at der kan laves sql injection
+EXEC ValidateLoginSQL @Name = 'Mads''--', @Password = 'hest' --Usikker, return 1 SQL injection er en succes
+GO
+--Giv adgang til SP
+GRANT EXECUTE ON dbo.ValidateLoginSQL TO [webtester]
+--test adgang som webtester
+EXECUTE AS USER = 'webtester'
+
+EXEC dbo.ValidateLoginSQL @Name = 'Mads', @Password = 'password' --return: The SELECT permission was denied
+
+REVERT
+GO
+--Vi kan løse ovenstående ved at give webtester select adgang til users
+GRANT SELECT ON dbo.[users] TO [webtester]
+--prøv igen
+--men nu er følgende også muligt!
+EXECUTE AS USER = 'webtester'
+SELECT * FROM [users]
+REVERT
+--Det er ikke meningen så lad os revoke rettighederne igen.
+REVOKE SELECT ON dbo.[users] TO [webtester]
+
+--Test 3 EXECUTE SP as OWNER
+CREATE OR ALTER PROCEDURE ValidateLoginAsOwner
+	@Name VARCHAR(100),
+	@Password VARCHAR(100) WITH EXECUTE AS OWNER
+AS
+	DECLARE @SQL NVARCHAR(4000) = '';
+	SELECT @SQL = 'SELECT COUNT(*) FROM [users] WHERE [Name] = ''' + @Name + ''' AND [Password] = HASHBYTES(''SHA2_512'',''' + @Password + ''')';
+	PRINT @SQL
+    EXEC sp_executesql @SQL;
+GO
+GRANT EXECUTE ON dbo.ValidateLoginAsOwner TO [webtester]
+--test adgang som webtester
+EXECUTE AS USER = 'webtester'
+
+EXEC dbo.ValidateLoginAsOwner @Name = 'Mads', @Password = 'password' --rigtig: return 1
+EXEC dbo.ValidateLoginAsOwner @Name = 'Mads'';SELECT * FROM [secrets] --', @Password = 'password' --rigtig. SQL injection succesfull
+
+REVERT
+
+--Test 4 Lav et schema der er ejet af sin egen bruger, der kun har de rettigheder den skal bruge
+GO
+create schema www
+GO
+ALTER USER webtester WITH DEFAULT_SCHEMA = www
+
+CREATE USER ApiExec WITHOUT LOGIN WITH DEFAULT_SCHEMA = www; 
+ALTER AUTHORIZATION ON SCHEMA::www TO ApiExec;
+GRANT SELECT ON dbo.[users] TO ApiExec
+GO
+CREATE OR ALTER PROCEDURE www.ValidateLoginWWW
+	@Name VARCHAR(100),
+	@Password VARCHAR(100) WITH EXECUTE AS OWNER
+AS
+	DECLARE @SQL NVARCHAR(4000) = '';
+	SELECT @SQL = 'SELECT COUNT(*) FROM [users] WHERE [Name] = ''' + @Name + ''' AND [Password] = HASHBYTES(''SHA2_512'',''' + @Password + ''')';
+	PRINT @SQL
+    EXEC sp_executesql @SQL;
+GO
+GRANT EXECUTE ON www.ValidateLoginWWW TO webtester
+GO
+--test adgang som webtester
+EXECUTE AS USER = 'webtester'
+
+EXEC www.ValidateLoginWWW @Name = 'Mads', @Password = 'password' --return 1
+EXEC www.ValidateLoginWWW @Name = 'Mads''--', @Password = 'password' --SQL injection succesfull
+EXEC www.ValidateLoginWWW @Name = 'Mads'';SELECT * FROM [secrets] --', @Password = 'password' -- SQL injection succesfull...men ikke adgang til secrets
+
+REVERT
+GO
+--Test 5 SP med parameter i det nye schema
+CREATE OR ALTER PROCEDURE www.ValidateLoginWeb
+	@Name VARCHAR(100),
+	@Password VARCHAR(100)
+AS
+	SELECT count(*) FROM [users] WHERE [Name] = @Name AND [Password] = HASHBYTES('SHA2_512', @Password)
+GO
+GRANT EXECUTE ON www.ValidateLoginWeb TO webtester
+GO
+--test adgang som webtester
+EXECUTE AS USER = 'webtester'
+
+EXEC www.ValidateLoginWeb @Name = 'Mads', @Password = 'password' --The SELECT permission was denied
+EXEC www.ValidateLoginWeb @Name = 'Mads''--', @Password = 'hest' --SQL injection virker ikke.
+
+REVERT
+GO
+--update SP to execute as owner
+CREATE OR ALTER PROCEDURE www.ValidateLoginWeb
+	@Name VARCHAR(100),
+	@Password VARCHAR(100)  WITH EXECUTE AS OWNER
+AS
+	SELECT count(*) FROM [users] WHERE [Name] = @Name AND [Password] = HASHBYTES('SHA2_512', @Password)
+GO
+--Prøv igen
+
+--Hvad er best practice?
---
+++ MSSQL-bluehat-hacking/xss-attack.sql
@@ -0,0 +1,94 @@
+﻿/************************************************************************************************/
+--forebyg at der gemmes XSS attack i databasen
+--eks.:
+CREATE TABLE [homepageusers](
+	[Id] [INT] IDENTITY(1,1) PRIMARY KEY,
+	[Name] [varchar](100) NOT NULL,
+	[Phone] [varchar](100) NOT NULL)
+GO
+
+--Problem:
+INSERT INTO [homepageusers]([Name], [Phone]) 
+VALUES ('<script>alert("unsafe & safe Utf8CharsDon''tGetEncoded ÄöÜ - Conex")</script>',
+		'<script>alert(87654321)</script>')
+--Dette vil potentielt returnere et java script direkte til browseren, hvor det vil blive afviklet
+SELECT * FROM [homepageusers]
+
+/************************************************************************************************/
+--Metode 1 Brug de rigtig datatyper og gør dem så små som mulige.
+--**Denne metode bør altid være en del af løsningen**
+--Husk at dette typisk kræver specificering i krav specifikationen
+DROP TABLE [homepageusers]
+CREATE TABLE [homepageusers](
+	[Id] [INT] IDENTITY(1,1) PRIMARY KEY,
+	[Name] [varchar](40) NOT NULL,
+	[Phone] [int] NOT NULL)
+GO
+INSERT INTO [homepageusers]([Name], [Phone]) 
+VALUES ('<script>alert("unsafe")</script>', 87654321)
+INSERT INTO [homepageusers]([Name], [Phone]) 
+VALUES ('Navn Navnesen', 87654321)
+--Det er stadig muligt med XSS, men mulighederne er blevet mindre
+SELECT * FROM [homepageusers]
+
+/************************************************************************************************/
+--Metode 2 Tillad kun de tegn der skal bruges
+ALTER TABLE [homepageusers]
+ADD CONSTRAINT stdName CHECK([Name] NOT LIKE '%[^ A-Za-z]%');
+--Første insert vil fejl da den indeholder andre chars end A-Za-z
+INSERT INTO [homepageusers]([Name], [Phone]) 
+VALUES ('<script>alert("unsafe")</script>',87654321)
+
+--Fjern constraint. Nødvendig for de følgende metoder
+ALTER TABLE [homepageusers]
+DROP CONSTRAINT stdName
+
+/************************************************************************************************/
+--Metode 3 Hvis det skal være muligt at indsætte special tegn. Overvej at html encode feltet via en stored procedure
+ALTER TABLE [homepageusers]
+ALTER COLUMN [Name] varchar(50) NOT NULL
+GO
+CREATE OR ALTER PROCEDURE [AddWebUser]  
+@Name AS VARCHAR(40),
+@Phone AS INT WITH EXECUTE AS OWNER
+AS 
+BEGIN
+	--validate param Name
+	DECLARE @htmlEncode varchar(120)
+	SELECT @htmlEncode = (SELECT @Name FOR XML PATH(''))
+	PRINT @htmlEncode
+	INSERT INTO [homepageusers] ([Name], [Phone]) VALUES (@htmlEncode, @Phone)
+END
+GO
+--TEST
+EXECUTE [AddWebUser] @Name = '<script>alert("unsafe")</script>', @Phone = 87654321
+SELECT * FROM [homepageusers]
+--Ulemper: der skal være en SP til update også. Det virker kun på det data der kommer igennem SP
+
+/************************************************************************************************/
+--Metode 4 en trigger på insert, update der encoder feltet...
+ALTER TABLE [homepageusers]
+ALTER COLUMN [Name] varchar(50) NOT NULL
+GO
+CREATE TRIGGER [homepageusersEncodeName]
+ON [homepageusers] 
+AFTER INSERT, UPDATE
+AS
+BEGIN
+	UPDATE [homepageusers]
+    SET [Name] = (SELECT i.[Name] AS [text()] FOR XML PATH(''))
+    FROM [homepageusers]
+    INNER JOIN inserted i on i.Id = [homepageusers].Id
+END
+GO
+
+--test
+INSERT INTO [homepageusers]([Name], [Phone]) 
+VALUES ('<script>alert("unsafe")</script>',87654321)
+SELECT * FROM [homepageusers]
+
+UPDATE [homepageusers]
+SET [Name] = '<script>alert("Hej")</script>'
+WHERE [Id] = 1
+SELECT * FROM [homepageusers]
+GO
